bed4eec5d9a711b8997c447a112d58839b5c9f35
aakashraj7 <aakashrajs1206@gmail.com>
2026-04-29 18:45:50 +0530

    user and address schema created

---
+++ backend/schema/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    name: {
+        type: String
+    },
+    age: {
+        type: Number,
+    },
+    gender: {
+        type: String,
+    },
+    stored_addresses: {
+        type: [Schema.Types.ObjectId], 
+        ref: "address" },
+    defaultAddress : {
+        type: Schema.Types.ObjectId,
+        ref: "add,ress"
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    }
+});
+
+const userModel = mongoose.model("users",userSchema);
+module.exports = userModel;
