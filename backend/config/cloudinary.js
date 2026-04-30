4851650b72fe9faf172cece2e1ceedd0cb1368a7
vakeer56 <mailvarun17@gmail.com>
2026-04-30 12:05:30 +0530

    added cloud configs & multer middleware

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,15 @@
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+
+
+cloudinary.config(
+    {
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUD_API_KEY,
+        api_secret: process.env.CLOUD_API_SECRET
+    }
+)
+
+console.log(process.env.CLOUD_NAME);
+
+module.exports = cloudinary;
---
+++ backend/middleware/upload.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        //stores the incoming file in /uploads in mem
+        cb(null, '/uploads');
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
